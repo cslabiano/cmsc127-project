de67f701bab5f78bda1f2c389ad0bac16dba6198
cslabiano <cslabiano@up.edu.ph>
2024-06-02 00:16:12 +0800

    feat: add item endpoints

--- server/index.js
+++ server/index.js
@@ -21,6 +21,8 @@ db.connect((err) => {
   console.log("Connected to the MySQL server.");
 });
 
+/*************** IUSER TABLE ***************/
+
 app.get("/users", (req, res) => {
   const sql = "SELECT * FROM user";
   db.query(sql, (err, data) => {
@@ -44,6 +46,88 @@ app.post("/users", (req, res) => {
       if (err) return res.status(500).json({ error: err });
       return res.status(201).json({ message: "User registered successfully" });
     });
+  });
+});
+
+/*************** ITEM TABLE ***************/
+
+// create or add item
+app.post("/items", (req, res) => {
+  const { price, name, description, estab_id, classifications } = req.body;
+
+  // insert item into the item table
+  const insertItemSql =
+    "INSERT INTO item (price, name, description, estab_id) VALUES (?, ?, ?, ?)";
+  db.query(
+    insertItemSql,
+    [price, name, description, estab_id],
+    (err, results) => {
+      if (err) return res.status(500).json({ error: err });
+
+      const item_id = results.insertId;
+
+      // insert classifications into the item_classification table
+      if (classifications && classifications.length > 0) {
+        const insertClassificationSql =
+          "INSERT INTO item_classification (item_id, classification) VALUES ?";
+        const classificationValues = classifications.map((classification) => [
+          item_id,
+          classification,
+        ]);
+        db.query(
+          insertClassificationSql,
+          [classificationValues],
+          (err, results) => {
+            if (err) return res.status(500).json({ error: err });
+            return res.status(201).json({ message: "Item added successfully" });
+          }
+        );
+      } else {
+        return res.status(201).json({ message: "Item added successfully" });
+      }
+    }
+  );
+});
+
+// read all item from establishment
+app.get("/:estab_id/items", (req, res) => {
+  const { estab_id } = req.params;
+  const sql = "SELECT * FROM item WHERE estab_id = ?";
+  db.query(sql, [estab_id], (err, data) => {
+    if (err) return res.json(err);
+    return res.json(data);
+  });
+});
+
+// read or search items
+app.get("/items/search", (req, res) => {
+  const { search_term } = req.query;
+  const sql = "SELECT * FROM item WHERE name LIKE '%${search_term}%';";
+  db.query(sql, (err, data) => {
+    if (err) return res.status(500).json({ error: err });
+    return res.json(data);
+  });
+});
+
+// update item
+app.put("/items/:item_id", (req, res) => {
+  const { item_id } = req.params;
+  const { price, name, description } = req.body;
+  const sql =
+    "UPDATE item SET price = ?, name = ?, description = ? WHERE item_id = ?";
+  db.query(sql, [price, name, description, item_id], (err, results) => {
+    if (err) return res.status(500).json({ error: err });
+    return res.json({ message: "Item updated successfully" });
+  });
+});
+
+// delete item
+app.delete("/items/:item_id", (req, res) => {
+  const { item_id } = req.params;
+  const sql = "DELETE FROM item WHERE item_id = ?";
+  db.query(sql, [item_id], (err, results) => {
+    if (err) return res.status(500).json({ error: err });
+    return res.json({ message: "Item deleted successfully" });
   });
 });
 
