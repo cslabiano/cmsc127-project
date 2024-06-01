ba6ecfa1806964740d1e95fb062fe105f3607117
luis-dm <lfdemesa1@up.edu.ph>
2024-06-01 17:38:18 +0800

    added foos estab img link to src of figure img

--- frontend/src/components/KusinaBox.js
+++ frontend/src/components/KusinaBox.js
@@ -20,10 +20,7 @@ const KusinaBox = (props) => {
     <>
       <div className="card w-96 shadow-xl bg-kusinasecondary text-kusinaprimary scale-75">
         <figure>
-          <img
-            src="https://cdn.vox-cdn.com/thumbor/5d_RtADj8ncnVqh-afV3mU-XQv0=/0x0:1600x1067/1200x900/filters:focal(672x406:928x662)/cdn.vox-cdn.com/uploads/chorus_image/image/57698831/51951042270_78ea1e8590_h.7.jpg"
-            alt={`Photo of  ${props.name}`}
-          />
+          <img src={props.image} alt={`Photo of  ${props.name}`} />
         </figure>
         <div className="card-body font-sans">
           <h2 className="card-title font-sans text-xl font-extrabold">
